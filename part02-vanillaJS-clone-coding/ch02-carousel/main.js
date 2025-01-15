2052d058eb96bcec6aab2a9c8d5db4c9176bcc1e
Eastyn <eastynwork@gmail.com>
2025-01-15 21:14:23 +0900

    feat($carousel - makeCarousel) - let the function be availabe to set images with the specified ones - Ch2. Carousel(기능 확장하기- 1)

--- part02-vanillaJS-clone-coding/ch02-carousel/main.js
+++ part02-vanillaJS-clone-coding/ch02-carousel/main.js
@@ -10,4 +10,9 @@ height:250px;
 background-color:rgb(100,100,100);
 box-sizing: border-box`;
 
+app.appendChild(
+	makeCarousel(['./images/carousel_image_04.jpg', './images/carousel_image_01.jpg', './images/carousel_image_02.jpg', './images/carousel_image_03.jpg'])
+);
+app.appendChild(makeCarousel(['./images/carousel_image_08.jpg', './images/carousel_image_09.jpg', './images/carousel_image_10.jpg']));
+app.appendChild(makeCarousel());
 app.appendChild(makeCarousel());
