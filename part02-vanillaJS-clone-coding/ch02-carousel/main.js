951824dbb1dcc4f28ba514db0dd4d1f3c1aa5397
Eastyn <eastynwork@gmail.com>
2025-01-16 22:18:01 +0900

    fix($carousel) - fix makeCarousel() to let the carousel show previous/next images when it moves (기능 확장하기 - 1)

--- part02-vanillaJS-clone-coding/ch02-carousel/main.js
+++ part02-vanillaJS-clone-coding/ch02-carousel/main.js
@@ -13,6 +13,8 @@ box-sizing: border-box`;
 app.appendChild(
 	makeCarousel(['./images/carousel_image_04.jpg', './images/carousel_image_01.jpg', './images/carousel_image_02.jpg', './images/carousel_image_03.jpg'])
 );
-app.appendChild(makeCarousel(['./images/carousel_image_08.jpg', './images/carousel_image_09.jpg', './images/carousel_image_10.jpg']));
+app.appendChild(
+	makeCarousel(['./images/carousel_image_08.jpg', './images/carousel_image_09.jpg', './images/carousel_image_10.jpg', './images/carousel_image_11.jpg'])
+);
 app.appendChild(makeCarousel());
 app.appendChild(makeCarousel());
--- part02-vanillaJS-clone-coding/ch02-carousel/module.js
+++ part02-vanillaJS-clone-coding/ch02-carousel/module.js
@@ -1,4 +1,4 @@
-export default function makeCarousel(itemList) {
+export default function makeCarousel(itemList, visibleCount = 3) {
 	const iconNext = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
 </svg>
@@ -7,6 +7,8 @@ export default function makeCarousel(itemList) {
 <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
 </svg>
 `;
+
+	const itemWidth = Math.trunc(700 / visibleCount);
 	const wrapper = createElement({
 		tagName: 'div',
 		properties: { id: 'wrapper' },
@@ -24,33 +26,57 @@ overflow: hidden;
 		properties: { id: 'itemContainer' },
 	});
 	itemContainer.style.cssText = `display:flex;
-transform:translate(-700px);`;
+transform:translateX(0)px);`;
 
 	addButtons();
 	// itemList가 있는 경우 새로 아이템을 만들어서 추가
 	if (itemList) {
+		// itemList = [...itemList.splice(-1), ...itemList];
 		itemList.forEach(item => {
-			itemList = [...itemList.splice(-1), ...itemList];
 			addImageItem(itemContainer, item);
 		});
 	} else {
 		// 없을 떄는 기존처럼 동작
-		addImageItem(itemContainer, './images/carousel_image_04.jpg');
 		addImageItem(itemContainer, './images/carousel_image_01.jpg');
 		addImageItem(itemContainer, './images/carousel_image_02.jpg');
 		addImageItem(itemContainer, './images/carousel_image_03.jpg');
+		addImageItem(itemContainer, './images/carousel_image_04.jpg');
 	}
 
 	function handleSlide(next = true) {
+		/* const clone = next ? itemContainer.firstChild.cloneNode(true) : undefined;
+		clone && itemContainer.appendChild(clone); */
+
+		if (next) {
+			itemContainer.appendChild(itemContainer.firstChild.cloneNode(true));
+		} else {
+			itemContainer.prepend(itemContainer.lastChild.cloneNode(true));
+			itemContainer.style.transform = `translateX(${-itemWidth}px)`; // 먼저 슬라이드 재배치
+		}
+
+		/* 
+    실행 시점 분리를 위해 setTimeout 사용 
+      왜? prev 버튼을 눌렀을 때, 인라인 스타일 태그는 마지막에 바꾼 애만 적용되었기 때문에 
+        이것(itemContainer.style.transform = `translateX(${-700 / visibleCount}px)`;)은 무시되고
+        얘(itemContainer.style.transform = `translateX(${next ? -700 / visibleCount : 0}px)`;)만 적용됨
+        => translateX(0)으로 보이기 때문에 ontransitionend의 코드도 동작하지 않음
+        => 그래서 실행시점을 분리한 것!
+    */
+		setTimeout(() => {
+			handleTransitionEnd(next);
+		}, 0);
+	}
+
+	function handleTransitionEnd(next) {
 		itemContainer.style.transitionDuration = '0.5s';
-		itemContainer.style.transform = `translateX(${next ? -1400 : 0}px)`;
+		itemContainer.style.transform = `translateX(${next ? -itemWidth : 0}px)`;
 
 		itemContainer.ontransitionend = () => {
 			// 제자리로 돌리기
 			itemContainer.style.removeProperty('transition-duration');
-			itemContainer.style.transform = 'translateX(-700px)';
+			itemContainer.style.transform = `translateX(0px)`;
 
-			next ? itemContainer.appendChild(itemContainer.firstChild) : itemContainer.prepend(itemContainer.lastChild);
+			next ? itemContainer.firstChild.remove() : itemContainer.lastChild.remove();
 		};
 	}
 
@@ -94,18 +120,20 @@ transform:translate(-700px);`;
 			parent,
 			properties: { className: 'item' },
 		});
-		container.style.cssText = `width: 700px;
+		container.style.cssText = `width: ${itemWidth}px;
 height: 250px;
 display: flex;
 justify-content: center;
 align-items: center;
-overflow: hidden;`;
+overflow: hidden;
+background-color: #000;`;
 
 		const image = createElement({
 			tagName: 'img',
 			parent: container,
 			properties: { src },
 		});
+		image.style.cssText = `height: 100%;`;
 
 		const caption = createElement({
 			tagName: 'span',
